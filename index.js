dddf38a14ea0296530b7ec205d5ea74136154030
ryanzola <ryanzola@me.com>
2018-12-08 15:51:48 -0500

    Callbacks, Labels, and Set - Implements labels - Implements callback functions - Implements passing arguments via onCallbackParams - Implements set function

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import {TweenMax, Power4, Bounce, TimelineMax} from "gsap/all";
+import { TweenMax, Power4, Bounce, TimelineMax } from 'gsap/all';
 
-/* 
+/*
  *  Tweening Basics
  *  Branch: feature/tweeningBasics
- */ 
+ */
 
 // TweenMax.to('#logo1', 1, {x: 850, y: 830, ease:
 // Power4.easeInOut});
@@ -27,22 +27,43 @@ import {TweenMax, Power4, Bounce, TimelineMax} from "gsap/all";
 
 // TweenMax.staggerFromTo(['#logo1', '#logo2', '#logo3'], 1, {y: 350}, {y: 830, ease: Bounce.easeOut}, 0.25);
 
-/* 
+/*
  *  Timeline Basics
  *  Branch: feature/timelineBasics
  */
 
- let masterTl = new TimelineMax();
- let servantTl = new TimelineMax()
+//  let masterTl = new TimelineMax();
 
- masterTl.to('#logo1', 1, {x: 850})
-         .to('#logo2', 1, {y: 830}, '-=0.5')
-         .add(animateLogo3, '-=0.5');
+//  masterTl.to('#logo1', 1, {x: 850})
+//          .to('#logo2', 1, {y: 830}, '-=0.5')
+//          .add(animateLogo3, '-=0.5');
 
-function animateLogo3() {
-  var logo3Tl = new TimelineMax();
+// function animateLogo3() {
+//   var logo3Tl = new TimelineMax();
 
-  logo3Tl.to('#logo3', 1, {x: '-=850'});
+//   logo3Tl.to('#logo3', 1, {x: '-=850'});
 
-  return logo3Tl;
+//   return logo3Tl;
+// }
+
+let masterTl = new TimelineMax();
+
+masterTl
+  .add('beginning') // label
+  .to('#logo1', 1, { x: 850 })
+  .add('endLogo1')
+  .to('#logo2', 1, { y: 830}, 'beginning+=0.5') // use label and apply an offset
+  .to('#logo3', 1, {
+    x: '-=850', 
+    onComplete: showBigLogo,
+    onCompleteParams: ['red']
+  }, '-=0.5');
+
+function showBigLogo(color) {
+  var bigLogo = document.querySelector('#bigLogo');
+
+  TweenMax.set(bigLogo, {
+    autoAlpha: 1,
+    fill: color
+  });
 }
