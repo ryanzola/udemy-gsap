ed85fd57145f16b0f647eefb467f33b9534b125a
ryanzola <ryanzola@me.com>
2018-12-08 14:54:50 -0500

    TweenMax Basics - Covers basic tweening functions   - TweenMax.to   - TweenMax.from   - TweenMax.fromTo   - TweenMax.staggerTo   - TweenMax.staggerFrom   - TweenMax.staggerFromTo

---
+++ index.js
@@ -0,0 +1,23 @@
+import {TweenMax, Power4, Bounce} from "gsap/all";
+
+// TweenMax.to('#logo1', 1, {x: 850, y: 830, ease:
+// Power4.easeInOut});
+
+// TweenMax.to('#logo1', 1, {x:850});
+// TweenMax.to('#logo1', 1, {y: 850, delay: 1});
+
+// TweenMax.from('#logo1', 1, {x: 850});
+
+// TweenMax.fromTo('#logo1', 1, {autoAlpha: 1, x: 0, y: 830}, {autoAlpha:0.2, x: 850, y: 830});
+
+// TweenMax.to('#logo1', 1, {y: 830});
+// TweenMax.to('#logo2', 1, {y: 830, delay: 0.25});
+// TweenMax.to('#logo3', 1, {y: 830, delay: 0.5});
+
+// TweenMax.to(['#logo1', '#logo2', '#logo3'], 1, {y: 830});
+
+// TweenMax.staggerTo(['#logo1', '#logo2', '#logo3'], 1, {y: 830}, 0.25);
+
+// TweenMax.staggerFrom(['#logo1', '#logo2', '#logo3'], 1, {y: 830}, 0.25);
+
+// TweenMax.staggerFromTo(['#logo1', '#logo2', '#logo3'], 1, {y: 350}, {y: 830, ease: Bounce.easeOut}, 0.25);
